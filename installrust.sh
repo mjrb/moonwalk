6726687110466f14760c7f150b8d2fef5b8449c7
mjrb <www.w.com123@gmail.com>
2019-05-10 11:41:40 -0400

    path error

--- installrust.sh
+++ installrust.sh
@@ -9,4 +9,4 @@ wget https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/r/rust-1.34.0-1.el7
 
 #install to local
 rpm2cpio cargo-1.34.0-1.el7.x86_64.rpm  | cpio -div
-rpm2cpio ../rust-1.34.0-1.el7.x86_64.rpm  | cpio -div
+rpm2cpio rust-1.34.0-1.el7.x86_64.rpm  | cpio -div
