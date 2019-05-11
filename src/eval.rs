f73c5d099e259424b42b00c7c5e14f3b96fc823a
aep000 <alexparson1@gmail.com>
2019-05-11 17:47:51 -0400

    Adding eval program without stack jumps

--- src/eval.rs
+++ src/eval.rs
@@ -26,10 +26,30 @@ impl Context {
         }
     }
 }
+//TODO: Check rules on deref
+pub fn deref_source(loc:&ast::Source, ctx: &Context) -> usize{
+    let location = source_to_val(loc, ctx);
+    ctx.mem[location]
+}
 
-pub fn deref_source(loc:&ast::Source, ctx: &Context) -> usize{ //IMPLIMENT
-    return 0;
-    //return ctx.mem[];
+
+pub fn deref_dest(loc:&ast::Dest, ctx: &Context) -> usize{
+    let location = match loc{
+        ast::Dest::Reg(reg) => get_reg_val(reg, ctx),
+        ast::Dest::Addr(loc) => ctx.mem[*loc],
+        ast::Dest::Deref(dst) => deref_dest(loc, ctx),//TODO deref_dest function
+    };
+    location
+}
+
+
+pub fn source_to_val(src: &ast::Source, ctx: &Context) -> usize{
+    match src{
+        ast::Source::Reg(reg) => get_reg_val(reg, ctx),
+        ast::Source::Addr(loc) => ctx.mem[*loc],
+        ast::Source::Literal(val) => *val,
+        ast::Source::Deref(src) => deref_source(src, ctx),//SHOULD I ERROR OR JUST REPEAT?
+    }
 }
 
 pub fn get_reg_val(reg: &ast::Register, ctx: &Context) -> usize{
@@ -41,6 +61,14 @@ pub fn get_reg_val(reg: &ast::Register, ctx: &Context) -> usize{
     }
 }
 
+pub fn set_reg_val(reg: &ast::Register, val: usize, ctx: &mut Context){
+    match reg{
+        ast::Register::A => ctx.a = val,
+        ast::Register::B => ctx.b = val,
+        ast::Register::C => ctx.c = val,
+        ast::Register::D => ctx.d = val,
+    }
+}
 // this is a separate eval for expressions it needs
 // to be separate so that an expression can recursively evaualted
 pub fn eval_expr(expr: &ast::Expr, ctx: &Context) -> bool {
@@ -48,20 +76,20 @@ pub fn eval_expr(expr: &ast::Expr, ctx: &Context) -> bool {
         ast::Expr::Backwards => !ctx.forward,
         ast::Expr::Forwards => ctx.forward,
         ast::Expr::Or(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::And(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::Gte(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::Lte(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::Gt(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::Lt(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::Eq(left, right) => eval_expr(left, &ctx) || eval_expr(right,&ctx),
-        ast::Expr::Lit(val) => {
-            match val{
-                ast::Source::Reg(reg) => get_reg_val(reg, &ctx)!=0,
-                ast::Source::Addr(loc) => ctx.mem[*loc] != 0,
-                ast::Source::Literal(val) => *val !=0,
-                ast::Source::Deref(loc) => deref_source(loc, ctx) != 0,
-            }
-        },
+        ast::Expr::And(left, right) => eval_expr(left, &ctx) && eval_expr(right,&ctx),
+        ast::Expr::Gte(left, right) => eval_expr(left, &ctx) >= eval_expr(right,&ctx),
+        ast::Expr::Lte(left, right) => eval_expr(left, &ctx) <= eval_expr(right,&ctx),
+        ast::Expr::Gt(left, right) => eval_expr(left, &ctx) > eval_expr(right,&ctx),
+        ast::Expr::Lt(left, right) => eval_expr(left, &ctx) < eval_expr(right,&ctx),
+        ast::Expr::Eq(left, right) => eval_expr(left, &ctx) == eval_expr(right,&ctx),
+        ast::Expr::Lit(val) => source_to_val(val, ctx) != 0,
+    }
+}
+
+pub fn jump_to_label(label: &String , ctx: &mut Context){
+    match ctx.labels.get(label){
+        Some(lineno) => ctx.pc = *lineno,
+        None =>(),
     }
 }
 
@@ -76,15 +104,70 @@ pub fn scan_labels(program: &Vec<ast::Line>) -> ScanResult {
     return ScanResult::Ok(HashMap::new());
 }
 
+
+pub fn execute_instruction(inst: &ast::Instruction, ctx: &mut Context) -> bool{
+    match inst{
+        ast::Instruction::Inc(dest, src) =>{
+            let srcval = source_to_val(src, ctx);
+            let destval = match dest{
+                ast::Dest::Reg(reg) => get_reg_val(reg, ctx),
+                ast::Dest::Addr(loc) => ctx.mem[*loc],
+                ast::Dest::Deref(dst) => ctx.mem[deref_dest(dest, ctx)],//TODO deref_dest function
+            };
+            let mut newval = 0;
+            if(ctx.forward){
+                newval = destval + srcval;
+            }
+            else{
+                newval = destval - srcval;
+            }
+            match dest{
+                ast::Dest::Reg(reg) => set_reg_val(reg, newval, ctx),
+                ast::Dest::Addr(loc) => ctx.mem[*loc] = newval,
+                ast::Dest::Deref(dst) => ctx.mem[deref_dest(dest, ctx)] = newval,//TODO deref_dest function
+            }
+
+
+            false
+        },
+        ast::Instruction::Jump(lbl) =>{
+            if(ctx.forward){
+                match lbl{
+                    Some(label) => jump_to_label(label,ctx),
+                    None => (), //Pop Stack and go
+                }
+            }
+            false
+        },
+        ast::Instruction::From(lbl) =>{
+            if(!ctx.forward){
+                match lbl{
+                    Some(label) => jump_to_label(label,ctx),
+                    None => (), //Pop Stack and go
+                }
+
+            }
+            false
+        },
+        ast::Instruction::Halt => true,
+    }
+}
+
+
 // evaluate a program
 pub fn eval(program: Vec<self::ast::Line>, ctx: &mut Context) {
     loop{
         let mut halted = false;
         let current_line = &program[ctx.pc];
-
-
-
-
+        //Check for option here!!
+        match(&current_line.cond){
+            Some(cond) => {
+                if(eval_expr(cond, &ctx)){
+                    halted = execute_instruction(&current_line.inst, ctx);
+                }
+            },
+            None => halted = execute_instruction(&current_line.inst, ctx), //DO Instruction
+        }
         let end_of_program = (ctx.pc > program.len() && ctx.forward) || (ctx.pc == 0 && !ctx.forward);
         if(halted || end_of_program){
             break;
